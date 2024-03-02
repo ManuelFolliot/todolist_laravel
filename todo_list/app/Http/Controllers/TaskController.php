54526d1bc9658776393a225b23323afc7c953bf5
Manuel Folliot <manuel.folliot@gmail.com>
2024-03-02 11:14:15 +0100

    installation des packages

--- todo_list/app/Http/Controllers/TaskController.php
+++ todo_list/app/Http/Controllers/TaskController.php
@@ -6,7 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class TaskController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return view('todo.index');
+    }
 }
